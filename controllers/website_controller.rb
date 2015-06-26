ca482ed78ab261facd90a3580c15fbe2fecfb109
Manuel Hutter <git@mhutter.net>
2015-06-26 15:37:29 +0200

    added update news

--- controllers/website_controller.rb
+++ controllers/website_controller.rb
@@ -1,6 +1,7 @@
 require 'controllers/application_controller'
 
 class WebsiteController < ApplicationController
-  get ('/') { slim :home }
-  get ('/test') { binding.pry; 'foo' }
+  get('/') { slim :home }
+  get('/updates') { slim :website_updates }
+  get('/test') { binding.pry; 'foo' }
 end

6e6ae766192533b2a6dee4c99efccdff1727101b
Manuel Hutter <git@mhutter.net>
2015-07-27 17:02:01 +0200

    added bar chart for eFaktura stats

--- controllers/statistik_controller.rb
+++ controllers/statistik_controller.rb
@@ -8,8 +8,42 @@ class StatistikController < ApplicationController
   end
 
   get('/statistik/efaktura.?:format?') do
+    @rows = Statistik.efaktura.to_a
     @columns = ['Jahr', 'Monat', 'GD', 'Anz. Rechnungen']
-    render_stats(:efaktura, 'eFaktura-Volumen', format)
+
+    case format
+    when 'xlsx'
+      return render_xlsx(@rows, @columns, 'eFaktura', 'eFaktura-Volumen')
+
+    when 'js'
+      @labels = @rows.map { |row| '%4d-%02d' % [row[:jahr], row[:monat]] }.uniq.sort
+      dataset_names = @rows.map { |row| row[:kanton] }.uniq
+      @dataset = {}
+      dataset_names.each do |kanton|
+        @dataset[kanton] = @labels.map { |l| [l, nil] }.to_h
+      end
+
+      @rows.each do |row|
+        label = '%4d-%02d' % [row[:jahr], row[:monat]]
+        @dataset[row[:kanton]][label] = row[:rechnungen]
+      end
+
+      @colors = {
+        'FR' => '#000000',
+        'GL' => '#FFD730',
+        'JU' => '#ffffff',
+        'SG' => '#009933',
+        'SZ' => '#E7423F',
+      }
+
+      headers 'Content-Type' => 'application/javascript'
+      return erb :'statistik/efaktura.js', layout: false
+
+    else # HTML
+
+      slim :'statistik/efaktura'
+
+    end
   end
 
   get('/statistik/aktive_le.?:format?') do
@@ -35,33 +69,33 @@ class StatistikController < ApplicationController
     @cols = @columns || @rows.first.keys
 
     if format.eql?('xlsx')
-
-      # generate XLSX File
-      p = Axlsx::Package.new
-      header = p.workbook.styles.add_style b: true
-      p.workbook.add_worksheet(name: name.to_s) do |sheet|
-        # header row
-        sheet.add_row @cols, style: header
-
-        # insert data
-        @rows.each { |row| sheet.add_row(row.values) }
-
-        # call any custom code if present
-        yield sheet if block_given?
-      end
-
-      filename = Date.today.strftime("#{file}_%Y-%m-%d.xlsx")
-      headers \
-        'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; char    set=UTF-8',
-        'Content-Disposition' => "attachment; filename=#{filename}"
-
-      return p.to_stream # rack can read a stream directly
-
+      return render_xlsx(@rows, @cols, name, file)
     else
-
       # Default is HTML
       slim :"statistik/#{name}"
-
     end
+  end
+
+  def render_xlsx(rows, cols, name, file)
+    # generate XLSX File
+    p = Axlsx::Package.new
+    header = p.workbook.styles.add_style b: true
+    p.workbook.add_worksheet(name: name.to_s) do |sheet|
+      # header row
+      sheet.add_row cols, style: header
+
+      # insert data
+      rows.each { |row| sheet.add_row(row.values) }
+
+      # call any custom code if present
+      yield sheet if block_given?
+    end
+
+    filename = Date.today.strftime("#{file}_%Y-%m-%d.xlsx")
+    headers \
+      'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; char    set=UTF-8',
+      'Content-Disposition' => "attachment; filename=#{filename}"
+
+    p.to_stream # rack can read a stream directly
   end
 end

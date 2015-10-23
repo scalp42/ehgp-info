bb2c3e393575e2f7396c6a5cb9679b4c7741263a
Manuel Hutter <git@mhutter.net>
2015-10-23 11:40:05 +0200

    Only count eFaktura Contracts

--- models/spital.rb
+++ models/spital.rb
@@ -41,9 +41,8 @@ class Spital < Base
         ', "sp_Aktiv" as aktiv',
         ', count ("vt_sp_ID") as vertraege',
         'from KOGU."StammSpital"',
-        'left outer join KOGU."VertragMandantMitSpital" on "vt_sp_ID" = "sp_ID"',
-        'where "sp_cd_LeXmlZertifiziert" = 1',
-        'and "sp_Aktiv" < 50',
+        'left outer join KOGU."VertragMandantMitSpital" on "vt_sp_ID" = "sp_ID" and "vt_cd_LeMdFaktura" = 1',
+        'where "sp_Aktiv" < 50 and "sp_cd_LeXmlZertifiziert" = 1',
         'group by "sp_ID","sp_Kanton", "sp_Name", "sp_Ort", "sp_Aktiv", "sp_cd_LeXmlZertifiziert"',
         'order by "sp_Kanton", "sp_Name"'
 
@@ -59,9 +58,8 @@ class Spital < Base
         ', "sp_Aktiv" as aktiv',
         ', count ("vt_sp_ID") as vertraege',
         'from KOGU."StammSpital"',
-        'inner join KOGU."VertragMandantMitSpital" on "vt_sp_ID" = "sp_ID"',
+        'inner join KOGU."VertragMandantMitSpital" on "vt_sp_ID" = "sp_ID" and "vt_cd_LeMdFaktura" = 1',
         'where ("sp_cd_LeXmlZertifiziert" != 1 or "sp_Aktiv" > 50)',
-        'and "vt_cd_LeMdFaktura" = 1',
         'group by "sp_ID","sp_Kanton", "sp_Name", "sp_Ort", "sp_Aktiv", "sp_cd_LeXmlZertifiziert"',
         'order by "sp_Kanton", "sp_Name"'
 
